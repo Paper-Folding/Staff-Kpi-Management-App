51569f6ba1dae98f40ae9a5fd7234497256ec3bd
Paper-Folding <907914149@qq.com>
2022-02-16 16:38:25 +0800

    added cors configuration

--- src/main/java/ndky/paper/kpimgrapp/StaffKpiManagementAppBackApplication.java
+++ src/main/java/ndky/paper/kpimgrapp/StaffKpiManagementAppBackApplication.java
@@ -3,10 +3,23 @@ package ndky.paper.kpimgrapp;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @MapperScan("ndky.paper.kpimgrapp.Mappers")
 @SpringBootApplication
 public class StaffKpiManagementAppBackApplication {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(StaffKpiManagementAppBackApplication.class, args);
