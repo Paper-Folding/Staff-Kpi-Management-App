5fc349102438d4b84efa794d6faa57d857b4eed6
Paper-Folding <907914149@qq.com>
2022-04-06 23:52:40 +0800

    fix bug caused by disabling staff info

--- src/main/resources/triggers.sql
+++ src/main/resources/triggers.sql
@@ -8,6 +8,7 @@ begin
     delete from role_scope where role_id = OLD.id;
 end;
 
+-- add default authentication with password 123456 after staff info added
 create trigger after_staff_insert
     after insert
     on staff_info
@@ -17,10 +18,31 @@ begin
     values (NEW.id, NEW.no, '$2a$10$on2WJ2SbqiwXeLJLnnaoGuZN7H2/cT5ay/DX9t8IwmwblyhAWkYSq'); -- 123456
 end;
 
+-- add initial role after authentication added
 create trigger after_authentication_insert
     after insert
     on authentication
     for each row
 begin
     insert into user_roles(authentication_id, role_id) values (NEW.id, 3);
+end;
+
+-- delete authentication on staff disable
+create trigger before_staff_disable
+    before update
+    on staff_info
+    for each row
+begin
+    if NEW.disabled = '1' then
+        delete from authentication where staff_info_id = NEW.id;
+    end if;
+end;
+
+-- delete user roles on authentication delete
+create trigger before_authentication_delete
+    before delete
+    on authentication
+    for each row
+begin
+    delete from user_roles where authentication_id = OLD.id;
 end;
